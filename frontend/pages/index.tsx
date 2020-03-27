a8a73c98f985b0b90e77cc561b610bc153940d54
Lucien <ldjvalstar@gmail.com>
2020-03-27 13:01:32 +0100

    Added material ui library

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default class Index extends React.Component {
     render() {
         return (
-            <div>Welcome to Next.js!</div>
+            <Button color="primary">Hello World</Button>
         );
     }
 }
